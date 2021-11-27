dc2d9d6c16ea54d22f05891f00b14867ce57434b
arslansevval <e170503031@stud.tau.edu.tr>
2021-11-27 16:46:30 +0300

    Responsive Navbar eklendi.

--- src/views/Pages/LandingPage.vue
+++ src/views/Pages/LandingPage.vue
@@ -1,5 +1,48 @@
 <template>
+
 <div>
+    <base-nav
+      v-model="showMenu"
+      :transparent="true"
+      menu-classes="justify-content-end"
+      class="navbar-horizontal navbar-main navbar-top navbar-dark"
+      expand="lg"
+    >
+      <div slot="brand" class="navbar-wrapper">
+        <b-navbar-brand to="/">
+          <img src="img/theme/logo_employee.png">
+        </b-navbar-brand>
+      </div>
+
+     <template>
+       <div class="navbar-collapse-header">
+         <b-row>
+           <b-col cols="6" class="collapse-brand">
+             <router-link to="/">
+               <img src="img/brand/green.png">
+             </router-link>
+           </b-col>
+           <b-col cols="6" class="collapse-close">
+             <button type="button" class="navbar-toggler" @click="showMenu = false">
+               <span></span>
+               <span></span>
+             </button>
+           </b-col>
+         </b-row>
+       </div>
+         <b-navbar-nav  class="align-items-lg-center ml-lg-auto">
+           <b-nav-item to="/dashboard">
+               <i class="ni ni-circle-08"></i>
+               <span class="nav-link-inner--text">Login</span>
+           </b-nav-item>
+           <b-nav-item to="/register">
+               <i class="ni ni-circle-08"></i>
+               <span class="nav-link-inner--text">Login for Company</span>
+           </b-nav-item>
+
+       </b-navbar-nav>
+     </template>
+    </base-nav>
     <div class="btn-group">
         <div class="text-right">
             <router-link to="/login">
