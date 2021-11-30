0eaa59fcb317516c3c5bf4e6023b84c1df6b4c99
kemalberkeak10 <kemalberkeak10@gmail.com>
2021-11-30 14:18:58 +0300

    axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import DashboardPlugin from './plugins/dashboard-plugin';
 import App from './App.vue';
-
+import { appAxios } from './utils/appAxios';
+import Axios from 'axios';
+import { createApp } from './App.vue';
 // router setup
 import router from './routes/router';
-// plugin setup
+Axios.defaults.headers.common['expertin'] = 'Expert In'
+Axios.defaults.baseURL = 'https://localhost:44362'
+    // plugin setup
 Vue.use(DashboardPlugin);
-
+Vue.use(appAxios);
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
