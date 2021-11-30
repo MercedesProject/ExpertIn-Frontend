18716f4a549a8429d84ea9c5caf22dc2d6fa70ab
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2021-11-30 22:20:49 +0300

    Landing Page update
    Landing Page update(Bg,button...)

--- src/views/Pages/LandingPage.vue
+++ src/views/Pages/LandingPage.vue
@@ -1,24 +1,24 @@
 <template>
-
 <div>
-  <!--NavBar-->
-  <div class="main-content navbar-main ml-auto">
+
+  <!--NavBar-->  
+  <div class="navbar-main ml-auto">
     <base-nav
       v-model="showMenu"
       :transparent="true"
       menu-classes="justify-content-end"
       class="navbar-horizontal navbar-main navbar-top navbar-dark"
       expand="lg"  
-       style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2)" 
+      style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2)" 
      >
   
-      <div slot="brand" class="navbar-wrapper">
-        <b-navbar-brand to="/">
-          <img style="width:210px; height: 70px" src="img/brand/logo_employee.png">
-        </b-navbar-brand>
-      </div>
+   <div slot="brand" class="navbar-wrapper">
+     <b-navbar-brand to="/">
+       <img style="width:210px; height: 70px" src="img/brand/logo_employee.png">
+     </b-navbar-brand>
+   </div>
 
-     <template>
+     <template>  
        <div class="navbar-collapse-header" style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2);">
          <b-row>
            <b-col cols="6" class="collapse-brand">
@@ -34,116 +34,113 @@
            </b-col>
          </b-row>
        </div>
-         <b-navbar-nav  class="align-items-lg-center ml-lg-auto">
+        
+       <b-navbar-nav class="align-items-lg-center ml-lg-auto">
+
            <b-nav-item to="/login">
-           <i class="ni ni-circle-08" style="color: #32284d; text-shadow: 0px 0px 1px #ffffff;"></i>
-              <base-button style=
-                    "text-transform:capitalize;
-                    text-shadow: 0px 1px 1px #0B8F8812; 
-                    border-bottom: 2px solid #b2b2b2; 
-                    background-color: #ffffff; 
-                    border-radius: 10px; 
-                    color: #32284d; 
-                    padding:10px 24px;
-                    border-color: #7e65c2; 
-                    height:auto; 
-                    width: auto" 
-                    outline type="login">Login</base-button>
+              <span class="nav-link-inner--text" style=
+                "font-size: 18px; 
+                 font-weight: bold;
+                 font-family: Nucleolcons; 
+                 text-transform:capitalize; 
+                 text-shadow: 0px 1px 1px #0B8F8812; 
+                 border-bottom: 1px solid #e5e0f2; 
+                 border-radius: 4px; 
+                 padding:10px 24px; 
+                 border-color: #9783ce;                  
+                 height:auto; 
+                 width: auto;"
+                 > Login </span>
            </b-nav-item>
-           <b-nav-item to="/logincompany">
-           <i class="ni ni-circle-08" style="color: #32284d; text-shadow: 0px 0px 1px #ffffff;"></i>
-              <base-button style=
-                    "text-transform:capitalize; 
-                    text-shadow: 0px 1px 1px #0B8F8812; 
-                    border-bottom: 2px solid #b2b2b2; 
-                    background-color: #ffffff; 
-                    border-radius: 10px; 
-                    color: #32284d; 
-                    padding:10px 24px; 
-                    border-color: #7e65c2; 
-                    height:auto; 
-                    width: auto" 
-                    outline type="logincompany">Login for Company</base-button>
+
+            <b-nav-item to="/logincompany">
+              <span class="nav-link-inner--text" style=
+                "font-size: 18px; 
+                 font-weight: bold;
+                 font-family: Nucleolcons; 
+                 text-transform:capitalize; 
+                 text-shadow: 0px 1px 1px #0B8F8812; 
+                 border-bottom: 1px solid #e5e0f2; 
+                 border-radius: 4px; 
+                 padding: 10px 24px; 
+                 border-color: #9783ce;                  
+                 height:auto; 
+                 width: auto;"
+                 > Login For Company</span>
            </b-nav-item>
        </b-navbar-nav>
      </template>
     </base-nav>
   </div>
-    
-    <!--Content-->
- <div class="body bg-gradient-busra py-7">
+ 
     <!-- Page content -->
-
-      <div class="container-fluid px-lg-5">
-        <b-container class="mt-auto" >
-          <b-row class="justify-content-center">
-            <div class="container-fluid " 
+      <div class="container-fluid px-lg-5" style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2)">
+        <b-container class="mt-mx-auto" >
+          <b-row class="justify-content">
+            <div class="container-fluid" 
             style="
-            min-height: auto; 
-            max-width: 100%;
+            min-height: 600px; 
+            width: 100%;
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: cover; 
-            background-position: center top;
-            background-image: linear-gradient(to top, rgba(255,255,255,0.4) 40%, rgba(255,255,255,0.9) 100%), url(img/brand/bg9.png) "
+            background-position: center center;
+            background-image: url(img/brand/bg19.png)"
           >  
-      
-          <div class="row justify-content-center text-center py-7">  
+
+          <div class="row justify-content-center text-center pt-7 pb-4">  
                <h1 style=
                 "font-size: 72px; 
                 font-family: Nucleolcons;
                 text-transform: uppercase; 
-                text-shadow: 2px -1px 0px #d8d0ec; 
+                text-shadow: 2px -1px 10px #d8d0ec; 
                 font-weight: bold; 
                 color: #191426; 
                 border-color: #7e65c2"
               > EXPERT IN</h1>
-
-          </div> 
-            <div class="btn-wrapper text-center">
+          </div>
+           
+            <div class="row justify-content-center text-center">
                 <h1 style=
                 "font-size: 48px; 
                 font-family: Nucleolcons;  
-                text-shadow: 0px 0px 1px #d8d0ec;
+                text-shadow: 1px 1px 1px #d8d0ec;
                 font-weight: bold; 
-                color: #64509b; 
+                color: #251e3a; 
                 position: relative;
                 border-color: #b1a2da"
                 > Finding a job is just a click away</h1>  
-
             </div>
 
             <div class="container-fluid">
-            <div class="row justify-content-center text-center"> 
-                <router-link to="/register">
-                <base-button style=
-                "font-size: 24px; 
-                font-weight: bold; 
-                background:linear-gradient(0deg, #0B8F8812,  #7E65C2); 
-                font-family: Nucleolcons;
-                text-shadow: 0px 1px 10px #ffffff; 
-                border-bottom: 1px solid #ffffff; 
-                border-radius: 10px;
-                padding:15px 30px; 
-                border-color: #b1a2da; 
-                height: auto; 
-                color: #191426; 
-                background-position: absolute;
-                width: auto" 
-                outline type="register" class="my-4 mt-mb-4">Explore Jobs</base-button>
-                </router-link>
+              <div class="row justify-content-center text-center pt-5"> 
+                  <router-link to="/register">
+                  <base-button style=
+                  "font-size: 26px; 
+                  font-weight: bold; 
+                  background:linear-gradient(180deg, #0B8F8812,  #7E65C2); 
+                  font-family: Nucleolcons;
+                  text-shadow: 0px 0px 5px #ffffff; 
+                  border: 1.8px solid #f2eff8; 
+                  border-radius: 10px;
+                  padding: 15px 25px; 
+                  border-color: #b1a2da; 
+                  height: auto; 
+                  color: #0c0a13; 
+                  background-position: absolute;
+                  width: auto" 
+                  outline type="register" class="my-3 mb-5">Explore Jobs</base-button>
+                  </router-link>
+              </div>     
             </div>
-            
-        </div>
-       
             </div>
           </b-row>
         </b-container>
       </div>
-  </div>
-   
+ 
     <!--Footer-->
-    <div class="footer bg-gradient-busra py-5" style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2)">
+    <div class="footer bg-gradient-busra" 
+    style="background:linear-gradient(135deg, #0B8F8812,  #7E65C2)">
       <div class="container-fluid">
         <div class="row text-center">
             <div class="col-12" style="color: #191426">© 2021 - TDU </div>
@@ -163,7 +160,5 @@
     </div>
 </div>
 </template>
-<script>
-
-</script>
+<script></script>
 <style></style>
